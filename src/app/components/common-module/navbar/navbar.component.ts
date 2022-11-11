807e5c0490954e5167af65d17b42b1c90b2c55a4
gonzalosinnott <gonzalo.sinnott@gmail.com>
2022-11-11 05:02:45 -0300

    fix navbar

--- src/app/components/common-module/navbar/navbar.component.ts
+++ src/app/components/common-module/navbar/navbar.component.ts
@@ -1,41 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { FirestoreService } from 'src/app/services/firestore.service';
+import { Auth } from '@angular/fire/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-
   userType: any;
 
-  constructor(private authService: AuthService, 
-              private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private readonly Auth: Auth,
+    public afAuth: AngularFireAuth,
+    private readonly firestore: FirestoreService
+  ) {}
 
   ngOnInit(): void {
     this.setUserType();
   }
 
   setUserType() {
-    var user = JSON.parse(localStorage.getItem('userData'));
-    if(user[0].role == 'Admin') {
-      this.userType = 'ADMINISTRADOR';
-    } else if(user[0].role == 'Patient') {
-      this.userType = 'PACIENTE';
-    } else if(user[0].role == 'Doctor') {
-      this.userType = 'PROFESIONAL';
-    }
+    this.afAuth.authState.subscribe((user) => {
+      if (user) {
+        this.firestore.getUserRole(this.Auth.currentUser.uid).then((role) => {
+          if (role == 'Admin') {
+            this.userType = 'ADMINISTRADOR';
+          }
+          if (role == 'Doctor') {
+            this.userType = 'PROFESIONAL';
+          }
+          if (role == 'Patient') {
+            this.userType = 'PACIENTE';
+          }
+          return 'Unknown';
+        });
+        return 'Unknown';
+      } else {
+        this.userType = 'Unknown';
+      }
+      return 'Unknown';
+    });
   }
-  
+
   logout() {
     sessionStorage.clear();
     localStorage.clear();
     this.authService
-        .logout()
-        .then(() => this.router.navigate(['/']))
-        .catch((e) => console.log(e.message));
+      .logout()
+      .then(() => this.router.navigate(['/']))
+      .catch((e) => console.log(e.message));
   }
-
 }
