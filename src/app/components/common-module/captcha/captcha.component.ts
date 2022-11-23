091767a205ba01b87d472b8db0bdbe863aa65f04
gonzalosinnott <gonzalo.sinnott@gmail.com>
2022-11-23 16:12:51 -0300

    sprint 5 - Captcha en directva

--- src/app/components/common-module/captcha/captcha.component.ts
+++ src/app/components/common-module/captcha/captcha.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-captcha',
@@ -7,42 +7,38 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class CaptchaComponent implements OnInit {
 
-  captcha: any = []
-  enteredCaptcha: any;
   @Output() captchaResult = new EventEmitter<boolean>();
+  captcha: any = [];
+  enteredCaptcha: any;
+  generateCaptcha: any;
+  captchaForDirective: any;
+
   constructor() { }
 
   ngOnInit(): void {
-    this.createCaptcha();
   }
 
   onChange(value: any) {
     if (value.length == 6) {
       setTimeout(() => {
-        this.validateCaptcha();
-      }, 200)
+        this.captchaForDirective = this.enteredCaptcha;
+        }, 200)
     }
   }
 
   createCaptcha() {
-    const activeCaptcha = document.getElementById("captcha");
-    let captcha = []
-    for (let q = 0; q < 6; q++) {
-      if (q % 2 == 0) {
-        captcha[q] = String.fromCharCode(Math.floor(Math.random() * 26 + 65));
-      } else {
-        captcha[q] = Math.floor(Math.random() * 10 + 0);
-      }
-    }
-    const theCaptcha = captcha.join("");
-    this.captcha = theCaptcha;
-    activeCaptcha!.innerHTML = `${theCaptcha}`;
+    console.log('createCaptcha')
+    this.generateCaptcha = true;
+    setTimeout(() => {
+      this.generateCaptcha = false;
+      }, 200)
   }
 
-  validateCaptcha() {
-    console.log(this.enteredCaptcha)
-    console.log(this.captcha)
-    this.captchaResult.emit(this.enteredCaptcha === this.captcha);
+  newCaptcha(captcha: any) {
+    this.captcha = captcha;
   }
 
+  captchaResultDirective(captchaResult) {
+    this.captchaResult.emit(captchaResult);
+  }  
 }
--- src/app/directives/captcha.directive.ts
+++ src/app/directives/captcha.directive.ts
@@ -1,16 +1,49 @@
-import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+import { Directive, EventEmitter, Input, Output, Renderer2, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[appCaptcha]'
 })
 export class CaptchaDirective {
 
-  @Input() captcha = '';
+  @Output() captcha = new EventEmitter<String>();
+  @Output() captchaResult = new EventEmitter<boolean>();
+  @Input() requestCaptcha = false;  
+  @Input() enteredCaptcha = false;
+  
+  theCatpcha: any;
 
-  constructor(private readonly elementRef: ElementRef,
-    private renderer: Renderer2) { }
+  constructor() { }
 
-  ngAfterViewInit(){
-    
+  ngOnInit(): void {
+    this.createCaptcha();
   }
+
+  createCaptcha() {
+    let captcha = []
+    for (let q = 0; q < 6; q++) {
+      if (q % 2 == 0) {
+        captcha[q] = String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+      } else {
+        captcha[q] = Math.floor(Math.random() * 10 + 0);
+      }
+    }
+    const theCaptcha = captcha.join("");
+    this.theCatpcha = theCaptcha;
+    this.captcha.emit(theCaptcha);
+  }
+
+  validateCaptcha() {
+    this.captchaResult.emit(this.enteredCaptcha === this.theCatpcha);
+  }  
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['requestCaptcha']) {
+      this.createCaptcha();     
+    }
+    
+    if (changes['enteredCaptcha']) {
+      this.validateCaptcha();
+    }
+  }
+
 }
